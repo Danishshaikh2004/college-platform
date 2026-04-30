78a9cab22f216b5cbee2dbdfa41ef5b330c02d6c
Danish Shaikh <skhdanish.8.4.5.2@gmail.com>
2026-04-30 21:29:23 +0530

    updated

--- frontend/app/colleges/[id]/page.tsx
+++ frontend/app/colleges/[id]/page.tsx
@@ -18,7 +18,7 @@ export default function CollegeDetail() {
   const [college, setCollege] = useState<College | null>(null);
 
   useEffect(() => {
-    fetch("http://localhost:5000/test-db")
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/test-db`)
       .then((res) => res.json())
       .then((data) => {
         const found = data.find((c: College) => c.id === Number(id));
